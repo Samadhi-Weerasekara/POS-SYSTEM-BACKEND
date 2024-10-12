e69bf9f20e885b7472e8d8542c2e0fbce8c1bd5e
sw799348 <sw799348@gmail.com>
2024-10-12 19:14:33 +0530

    customerNotFound exception updated

--- src/main/java/lk/ijse/posbackendspring/exception/CustomerNotFound.java
+++ src/main/java/lk/ijse/posbackendspring/exception/CustomerNotFound.java
@@ -1,4 +1,15 @@
 package lk.ijse.posbackendspring.exception;
 
-public class CustomerNotFound {
+public class CustomerNotFound extends RuntimeException {
+    public CustomerNotFound() {
+
+    }
+
+    public CustomerNotFound(String message) {
+        super(message);
+    }
+
+    public CustomerNotFound(String message, Throwable cause) {
+
+    }
 }
