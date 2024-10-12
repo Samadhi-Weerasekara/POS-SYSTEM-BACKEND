7f356aec37059f9b101eed4790d3dba450afb175
sw799348 <sw799348@gmail.com>
2024-10-12 19:29:29 +0530

    order entity and orderdetails entity updated

--- src/main/java/lk/ijse/posbackendspring/entity/OrderDetailEntity.java
+++ src/main/java/lk/ijse/posbackendspring/entity/OrderDetailEntity.java
@@ -1,6 +1,23 @@
 package lk.ijse.posbackendspring.entity;
 
-import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "order_details")
+@Entity
 public class OrderDetailEntity implements Serializable {
+    @Id
+    private String itemId;
+    private String itemName;
+    private int itemQty;
+    private double unitPrice;
+    private String itemImage;
 }
--- src/main/java/lk/ijse/posbackendspring/entity/OrderEntity.java
+++ src/main/java/lk/ijse/posbackendspring/entity/OrderEntity.java
@@ -1,4 +1,24 @@
 package lk.ijse.posbackendspring.entity;
 
-public class OrderEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "orders")
+@Entity
+public class OrderEntity implements Serializable {
+    @Id
+    private String orderId;
+    private String date;
+    private double total;
+    private String customerId;
+
 }
