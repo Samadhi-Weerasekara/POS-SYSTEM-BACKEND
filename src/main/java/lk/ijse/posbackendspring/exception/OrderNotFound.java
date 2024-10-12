64dcfbbd9b4b707f07efda8f30da6c44b920787e
sw799348 <sw799348@gmail.com>
2024-10-12 19:17:15 +0530

    orderNotFound exception updated

---
+++ src/main/java/lk/ijse/posbackendspring/exception/OrderNotFound.java
@@ -0,0 +1,15 @@
+package lk.ijse.posbackendspring.exception;
+
+public class OrderNotFound extends RuntimeException{
+    public OrderNotFound() {
+
+    }
+
+    public OrderNotFound(String message) {
+
+    }
+
+    public OrderNotFound(String message, Throwable cause) {
+
+    }
+}
